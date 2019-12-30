1682880be3892c7238119b3eefdec0cae404708e
Diana <dianar_8_8@hotmail.com>
2019-12-29 23:28:24 -0600

    "Subiendo proyecto"

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,18 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Diana Arana on 29/12/19.
+//  Copyright © 2019 Diana Arana. All rights reserved.
+//
+
+import Foundation
+
+struct Emoji {
+    
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
+
